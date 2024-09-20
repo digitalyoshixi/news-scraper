b1130a4e32f35764d521235f39b7fc0b14117786
yoshixi <davidchenyuhe@proton.me>
2024-09-19 21:47:58 -0400

    setup flask and venv

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__) # referencing this file
+
+@app.route('/')
+def index():
+    return render_template('index.html') 
+
+app.run(host="0.0.0.0", port=80) # run the app on localhost:80 running on all adddresses
+
