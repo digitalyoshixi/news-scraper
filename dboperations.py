ab18edc1f75634e1940a5bd2df595878a9a7831a
yoshixi <digitalyoshixi@proton.me>
2024-12-23 17:44:09 -0500

    database insert operation

--- dboperations.py
+++ dboperations.py
@@ -4,6 +4,7 @@ This file will handle the connection to the postgres database aswell as provide 
 import psycopg2
 import os
 from dotenv import load_dotenv
+import json
 load_dotenv()
 postgrespass = os.getenv('postgrespass')
 # Connect to the database
@@ -17,15 +18,16 @@ except Exception as e:
 # ---- CRUD Operations ----
 
 # Add Row to table
-def add_entry(url : str, author : list[str], content : dict, tags : list[str]) -> None:
-    with conn: # assuming we have connection
-    	with conn.cursor() as dbcurs:
-    	        try:
-    	            dbcurs.execute("""
-    		            INSERT INTO articles (username,password) VALUES
-    		            ('DANIEL','dogecoin'),
-    		            ('DEMON', 'dogworm7')
-    	            """)
-    	        except (Exception, psycopg2.DatabaseError) as error:
-    	            print(error)
-
+def add_entry(url: str, authors: dict, content: dict, tags: list) -> None:
+    with conn:  # assuming we have connection
+        with conn.cursor() as dbcurs:
+            try:
+                # Insert JSON object into the database
+                dbcurs.execute(f"""
+                    INSERT INTO articles (url, authors, content, tags) VALUES
+                    ('{url}', '{json.dumps(authors)}'::jsonb, '{json.dumps(content)}'::jsonb, '{json.dumps(tags)}');
+                """)
+            except (Exception, psycopg2.DatabaseError) as error:
+                print(error)
+# Example call to add_entry
+add_entry('marimo', {'1': "Dimiti", '2': "ZAMBAI"}, {'1': ["goldlock"]}, ['Homber'])
