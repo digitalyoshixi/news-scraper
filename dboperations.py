e124af5b9081aefe7b38c0627626bfb917bec3bc
yoshixi <digitalyoshixi@proton.me>
2024-12-23 17:23:59 -0500

    db connection ok

--- dboperations.py
+++ dboperations.py
@@ -7,9 +7,13 @@ from dotenv import load_dotenv
 load_dotenv()
 postgrespass = os.getenv('postgrespass')
 # Connect to the database
-conn = psycopg2.connect(f"postgresql://postgres.gqujynuglauuqaicuuvy:{postgrespass}@aws-0-us-east-1.pooler.supabase.com:6543/postgres")
+try:
+    conn = psycopg2.connect(f"postgresql://postgres.gqujynuglauuqaicuuvy:{postgrespass}@aws-0-us-east-1.pooler.supabase.com:6543/postgres")
+    print("Connected to the data base sucessfully!")
+except Exception as e:
+    print("Unable to connect to the database.")
+    print(e)
 
-print(conn)
 # ---- CRUD Operations ----
 
 # Add Row to table
@@ -18,7 +22,7 @@ def add_entry(url : str, author : list[str], content : dict, tags : list[str]) -
     	with conn.cursor() as dbcurs:
     	        try:
     	            dbcurs.execute("""
-    		            INSERT INTO users (username,password) VALUES
+    		            INSERT INTO articles (username,password) VALUES
     		            ('DANIEL','dogecoin'),
     		            ('DEMON', 'dogworm7')
     	            """)
